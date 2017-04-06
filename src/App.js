c9e714a91fa1160bbb1768727ed81b2d380df07d
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2017-04-06 16:16:35 -0400

    organized api data

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ const json = require("../data/apiKey.json");
 
 const URL_BASE = 'https://www.quandl.com/api/v3/datasets/';
 const DATABASE_CODE = 'WIKI';
-const DATASET_CODE = 'FB';
 const DATA_FORMAT = 'json';
 const URL_COLOMN = 'column_index';
 const URL_START = 'start_date';
@@ -15,46 +14,59 @@ const URL_COLLAPSE = 'collapse';
 const URL_TRANSFORM = 'transform';
 const URL_API = 'api_key';
 
+const DEFAULT_COMPANY = 'GOOG';
+
 class App extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      result: null,
+      data: null, //first column is date, second column is close
       apiKey: json.apiKey,
       column: '4',
       start_date: '2014-01-01',
       end_date: '2014-12-31',
       collapse: 'monthly',
-      transform: 'rdiff',
+      transform: 'none',
+      dataset_code: DEFAULT_COMPANY,
     }
 
     this.apiCall = this.apiCall.bind(this);
     this.setResult = this.setResult.bind(this);
   }
 
+  componentDidMount() {
+    this.apiCall();
+  }
+
   apiCall() {
-    const { apiKey, column, start_date, end_date, collapse, transform } = this.state
-    const url = `${URL_BASE}${DATABASE_CODE}/${DATASET_CODE}.${DATA_FORMAT}?${URL_COLOMN}=${column}&${URL_START}=${start_date}&${URL_END}=${end_date}&${URL_COLLAPSE}=${collapse}&${URL_TRANSFORM}=${transform}&${URL_API}=${apiKey}`;
+    const { apiKey, column, start_date, end_date, collapse, transform, dataset_code } = this.state
+    const url = `${URL_BASE}${DATABASE_CODE}/${dataset_code}.${DATA_FORMAT}?${URL_COLOMN}=${column}&${URL_START}=${start_date}&${URL_END}=${end_date}&${URL_COLLAPSE}=${collapse}&${URL_TRANSFORM}=${transform}&${URL_API}=${apiKey}`;
     fetch(url)
       .then(response => response.json())
       .then(result => this.setResult(result));
-      console.log(url);
   }
 
   setResult(result) {
-    console.log(result);
+    this.setState({
+      data: result.dataset.data,
+    });
   }
 
   render() {
-    //const {  }
+    const { data } = this.state;
+    console.log(data);
     return (
       <div>
         <CompanyButton 
           onClick={() => this.apiCall()}
+        >
+          Google
+        </CompanyButton>
+        <Graph 
+          data={data}
         />
-        <Graph />
         <Info />
       </div>
     );
@@ -63,13 +75,13 @@ class App extends Component {
 
 class CompanyButton extends Component {
   render() {
-    const { onClick } = this.props;
+    const { onClick, children } = this.props;
     return (
       <button
         type="button"
         onClick={onClick}
       >
-        Facebook
+        {children}
       </button>
     );
   }
@@ -77,6 +89,8 @@ class CompanyButton extends Component {
 
 class Graph extends Component {
   render() {
+    const { data } = this.props;
+
     return (
       <div> GRAPH </div>
     );
